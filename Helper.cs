697134c0b6b4b6bbe5719e6026d860d32eac4d9e
Bruna Rocha <133261652+brunatlrocha@users.noreply.github.com>
2024-02-11 23:00:57 -0500

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace lab3
+{
+    internal class Helper
+    {
+        private static Random random = new Random();
+
+        public static int[] GenerateIntArrayWithRandValues(int sizeOfArray)
+        {
+            int[] resultArray = new int[sizeOfArray];
+            for (int i = 0; i < sizeOfArray; i++)
+            {
+                resultArray[i] = random.Next(101);
+            }
+            return resultArray;
+        }
+
+        public static void PrintIntArrayToConsole(int[] arrayToPrint)
+        {
+            for (int i = 0; i < arrayToPrint.Length; i++)
+            {
+                Console.Write(arrayToPrint[i] + "\t");
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static int LinearSearch(int[] haystack, int needle, out int numOfComparison)
+        {
+            numOfComparison = 0;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                numOfComparison++;
+
+                if (haystack[i] == needle)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int BubbleSort(int[] arr)
+        {
+            int swapCount = 0;
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i - 1] > arr[i])
+                    {
+                        int temp = arr[i - 1];
+                        arr[i - 1] = arr[i];
+                        arr[i] = temp;
+                        swapped = true;
+                        swapCount++;
+                    }
+                }
+            } while (swapped);
+
+            return swapCount;
+        }
+
+        public static int BinarySearch(int[] haystack, int needle, out int numOfComparison)
+        {
+            numOfComparison = 0;
+            int low = 0;
+            int high = haystack.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                numOfComparison++;
+
+                if (haystack[mid] == needle)
+                {
+                    return mid;
+                }
+                else if (haystack[mid] < needle)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ lab3.cs
@@ -0,0 +1,73 @@
+﻿using lab3;
+using System;
+
+namespace Lab3
+{
+    class Lab3
+    {
+        static void Main()
+        {
+
+            int[] originalArray = Helper.GenerateIntArrayWithRandValues(100);
+
+
+            Console.WriteLine("Original Array:");
+            Helper.PrintIntArrayToConsole(originalArray);
+
+            int[] newArray = new int[originalArray.Length];
+            originalArray.CopyTo(newArray, 0);
+
+
+            int swapCount = Helper.BubbleSort(newArray);
+
+
+            Console.WriteLine($"Number of Swaps performed to sort the array: {swapCount}");
+
+
+            Console.WriteLine("Sorted Array:");
+            Helper.PrintIntArrayToConsole(newArray);
+
+            do
+            {
+
+                Console.Write("Enter an integer: ");
+                if (int.TryParse(Console.ReadLine(), out int userValue))
+                {
+
+                    int linearSearchIndex = Helper.LinearSearch(originalArray, userValue, out int linearSearchComparisons);
+
+                    if (linearSearchIndex != -1)
+                    {
+                        Console.WriteLine($"Linear Search: Value found at index {linearSearchIndex}. Comparisons: {linearSearchComparisons}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Linear Search: Value not found. Comparisons: {linearSearchComparisons}");
+                    }
+
+
+                    int binarySearchIndex = Helper.BinarySearch(newArray, userValue, out int binarySearchComparisons);
+
+                    if (binarySearchIndex != -1)
+                    {
+                        Console.WriteLine($"Binary Search: Value found at index {binarySearchIndex}. Comparisons: {binarySearchComparisons}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Binary Search: Value not found. Comparisons: {binarySearchComparisons}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+
+
+                Console.Write("Do you want to search another integer? (Y/N): ");
+            } while (Console.ReadLine()?.Trim().ToUpper() == "Y");
+
+
+            Console.WriteLine("Program terminated.");
+        }
+    }
+}
